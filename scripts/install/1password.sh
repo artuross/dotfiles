1f8d086e538c3be668df4331d4e1aaeea3270a6d
Artur Rychlewicz <8393226+artuross@users.noreply.github.com>
2024-01-25 17:19:54 +0100

    1password: add config for SSH agent

---
+++ scripts/install/1password.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Configures 1Password SSH agent settings.
+# Source: https://developer.1password.com/docs/ssh/agent/compatibility/#configure-ssh_auth_sock-globally-for-every-client
+
+# load config
+launchctl load -w ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
