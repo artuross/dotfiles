e886eff5cbd3508de67e4c14c2833485f2c06d02
Artur Rychlewicz <8393226+artuross@users.noreply.github.com>
2024-07-08 14:11:11 +0200

    asdf: rename script

--- home/.chezmoiscripts/run_after_asdf.sh
+++ home/.chezmoiscripts/run_after_asdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Installs asdf plugins and installs versions specified in .tool-versions.
+# Triggers on every `chezmoi apply`.
+#
+# TODO: trigger only when .tool-versions changes.
+
 asdf plugin add bun https://github.com/cometkim/asdf-bun
 asdf plugin add chezmoi https://github.com/joke/asdf-chezmoi.git
 asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
