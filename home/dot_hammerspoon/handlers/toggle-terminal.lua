5a7e2a6012dd45e01a1b2c8b446226c5e5d77a4d
Artur Rychlewicz <8393226+artuross@users.noreply.github.com>
2025-09-15 11:57:54 +0200

    Hammerspoon: only toggle Kitty, do not focus editor

--- home/dot_hammerspoon/handlers/toggle-terminal.lua
+++ home/dot_hammerspoon/handlers/toggle-terminal.lua
@@ -1,30 +1,12 @@
+-- Launches, or focuses Kitty if it's not an active window. Otherwise hides it.
+
 local handler = {}
 
-local prevApp
-
 function handler.Handler()
-    -- get previous app
-    if (prevApp == nil) then
-        prevApp = hs.application.frontmostApplication()
-    end
-
-    -- get current app
     local current = hs.application.frontmostApplication()
 
-    -- get prev app and save current as prev
-    local prev = prevApp
-    prevApp = current
-
-    -- what I most likely want is Kitty
-    if (current:name() == "kitty" and prev:name() == "kitty") then
-        current:hide()
-        hs.application.launchOrFocus("Zed")
-        return
-    end
-
     if (current:name() == "kitty") then
-        current:hide()
-        prev:activate()
+        local ok = current:hide()
         return
     end
 
