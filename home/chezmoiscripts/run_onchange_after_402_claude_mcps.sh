22588b1c5daf2de8aff59cc0cb3889b46414cb96
Artur Rychlewicz <8393226+artuross@users.noreply.github.com>
2025-09-19 13:20:23 +0200

    claude-code: fix chezmoi script paths

--- home/chezmoiscripts/run_onchange_after_402_claude_mcps.sh
+++ home/chezmoiscripts/run_onchange_after_402_claude_mcps.sh
@@ -3,5 +3,7 @@
 # Installs Claude Code MCP servers. In v1.0.119, the global config is stored in
 # ~/.claude.json and thus cannot be managed declaratively.
 
-~/.local/bin/claude mcp add context7 context7-mcp || true
-~/.local/bin/claude mcp add tree_sitter mcp-server-tree-sitter || true
+claudePath=$(/opt/homebrew/bin/mise where npm:@anthropic-ai/claude-code)/bin/claude
+
+$claudePath mcp add context7 context7-mcp || true
+$claudePath mcp add tree_sitter mcp-server-tree-sitter || true
